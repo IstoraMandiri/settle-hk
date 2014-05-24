4b5e6450c7825b4247d9f97c3b7b53e8909b4db8
Chris Hitchcott <hitchcott@gmail.com>
2014-05-24 15:19:40 +0800

    Scraping script to get all raw data + data map from Gazetteer.hk

---
+++ data-sci/scraping/gazetteer_scraper.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+"""
+Settle HK Gazetteer Scraper
+"""
+from __future__ import division
+import urllib2
+import os
+import pandas as pd
+import json
+
+DATA_DIR = 'data/'
+
+sources = [
+  {
+    'url':'http://137.189.97.90:5901/api/?projector=region,district,area,table,row,column,value&return=data&skip=0',
+    'output':'gazetteer_raw_data.json'
+  },
+  {
+    'url':'http://gazetteer.hk/locale/en-US/generated_ns-translation.json',
+    'output':'gazetteer_map.json'
+  }
+]
+
+for source in sources:
+
+  # Check if file exists already
+  if not os.path.isfile(DATA_DIR+source['output']):
+    print source['output'] + ' doesnt eixst, downloading...'
+    # If not, download data from the API
+    json_data = urllib2.urlopen(source['url'])
+
+    data = json.load(json_data)
+    if 'data' in data:
+      data = data['data']
+    with open(DATA_DIR+source['output'], 'w') as outfile:
+      json.dump(data, outfile)
+
+  else:
+    print source['output'] + ' already exists'
+
+print 'Completed'
+# print df.describe()
+# Read the the data, return a pandas summary
+
+
+
+
